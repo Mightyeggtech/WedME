bd28a7e8b9e26bebc04576004dbea04032ca26b4
mimsumaiya <sumaiyamim2050@gmail.com>
2020-02-10 17:08:38 +0600

    Worked on Vendors page 10th feb

--- src/components/allVendors.jsx
+++ src/components/allVendors.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Col } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import { Radio } from 'react'
+import { FormControl } from 'react-bootstrap';
+import { InputGroup } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
 import { Row } from "react-bootstrap";
 import { FiMapPin } from "react-icons/fi";
 import { FaStar } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
+import ReadAdviceAndIdeas from './readAdviceAndIdeas.jsx';
+
 function Vendors () {
     return (
         <React.Fragment>
@@ -138,10 +147,210 @@ function Vendors () {
                                 </Col>                                                              
                             </Row>
                         </Col>
-                        <Col xl={3}></Col>                        
+                        <Col xl={3} className="filter">
+                           <div>
+                               <div>
+                                    <Form >
+                                        <InputGroup className="mb-3">
+                                            <FormControl
+                                            placeholder="Search"
+                                            aria-label="Recipient's username"
+                                            aria-describedby="basic-addon2"
+                                            className="button-textbox"
+                                            />
+                                            <InputGroup.Append className="button-search">
+                                            <Button variant="outline-secondary"><FiSearch/></Button>
+                                            </InputGroup.Append>
+                                        </InputGroup>
+                                        <div className="pos-relative"><h3>VENDORS <span></span></h3></div>
+                                        <Form.Group controlId="exampleForm.ControlSelect1">
+                                            <Form.Label><div className="pos-relative"><h4>Dance & Music</h4></div></Form.Label>
+                                            <Form.Control as="select">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                            </Form.Control>
+                                        </Form.Group>
+                                        <Form.Group controlId="exampleForm.ControlSelect1">
+                                            <Form.Label><div className="pos-relative"><h4>Invites & Gifts</h4></div></Form.Label>
+                                            <Form.Control as="select">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                            </Form.Control>
+                                        </Form.Group>
+                                        <div className="pos-relative">
+                                            <h4>Photographers</h4>
+                                        </div>
+                                        <div className="pt-2">
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group1"/>
+                                                    <InputGroup.Text >Photographers</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup> 
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group2"/>
+                                                    <InputGroup.Text >Cinema/Video</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup> 
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group3"/>
+                                                    <InputGroup.Text >Pre Wedding Shoot</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup>
+                                        </div>
+                                        <div className="pos-relative">
+                                            <h4>Makeup</h4>
+                                        </div>
+                                        <div>
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group1"/>
+                                                    <InputGroup.Text >Bridal Makeup</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup> 
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group2"/>
+                                                    <InputGroup.Text >Family Makeup</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup> 
+                                        </div>
+                                        <div className="pos-relative">
+                                            <h4>Bridal Wear</h4>
+                                        </div>
+                                        <div>
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group1"/>
+                                                    <InputGroup.Text >Bridal Lehengas</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup> 
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group2"/>
+                                                    <InputGroup.Text >Kanjeevaram / Silk Sarees</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup> 
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group3"/>
+                                                    <InputGroup.Text >Cocktail Gowns</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup>
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group3"/>
+                                                    <InputGroup.Text >Trousseau Sarees</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup>
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group3"/>
+                                                    <InputGroup.Text >View All Bridal Wear</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup>                                                                                            
+                                        </div>
+                                        <div className="pos-relative">
+                                            <h4>Groom Wear</h4>
+                                        </div>
+                                        <div>
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group1"/>
+                                                    <InputGroup.Text >Sherwani</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup> 
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group2"/>
+                                                    <InputGroup.Text >Wedding Suits / Tuxes</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup>
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group2"/>
+                                                    <InputGroup.Text >View All Groom Wear</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup>                                              
+                                        </div>
+                                        <div className="pos-relative">
+                                            <h4>Planning & Decor</h4>
+                                        </div>
+                                        <div>
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group2"/>
+                                                    <InputGroup.Text >Wedding Planners</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup>
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Radio  name="group2"/>
+                                                    <InputGroup.Text >Decor</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                            </InputGroup>                                              
+                                        </div>                                                                                                                                                                 
+                                    </Form>
+                               </div>
+                           </div>
+                        </Col>                        
                     </Row>
+                    <Row className="VOVBS paT-80 paB-80">
+                        <Col lg={12}>
+                            <Row className="pb-5">
+                            <Col lg ={2} ></Col>
+                            <Col lg ={8}>
+                                <h1 className="text-center text-white font-uppercase">Read Advice & Ideas</h1>
+                                <p className="font-cormorant text-white  text-center">Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry.</p>
+                            </Col>
+                            <Col lg ={2} ></Col>    
+                        </Row>
+                        </Col>
+                        <Col lg ={6} xl ={4} className="my-2">
+                            <Card className="second-section-vendor-card p-4">
+                                <div className="cardIconFix"> 
+                                    <Card.Img src="/img/awesome-lightbulb.png"/>
+                                </div>
+                                <Card.Body>
+                                    <h3 className="text-center">Discover Wedding Ideas</h3>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col lg ={6} xl ={4} className="my-2">
+                            <Card className="second-section-vendor-card p-4">
+                                <div className="cardIconFix">
+                                    <Card.Img src="/img/material-event-note.png"/> 
+                                </div>
+                                <Card.Body>
+                                    <h3 className="text-center">Book Trusted Vendors</h3>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col lg ={6} xl ={4} className="my-2">
+                            <Card className="second-section-vendor-card p-4">
+                                <div className="cardIconFix">
+                                    <Card.Img src="/img/material-description.png"/>
+                                </div>
+                                <Card.Body>
+                                    <h3 className="text-center">Always Stay Organised</h3>
+                                </Card.Body>
+                            </Card>
+                        </Col>                     
+                    </Row> 
                 </section>
+
             </div>
+            <div class="container-fluid" id="homeReadAdviceAndIdeas">
+                <ReadAdviceAndIdeas/>
+            </div> 
         </React.Fragment>
     );
 }
